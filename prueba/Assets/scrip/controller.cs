7673d9629bf4e9c6e0148d6dcb53858a9345b99a
DennisAlejandro99 <alejandroibarrapr99@gmail.com>
2022-08-25 08:40:34 -0500

    version_5

--- prueba/Assets/scrip/controller.cs
+++ prueba/Assets/scrip/controller.cs
@@ -25,17 +25,22 @@ public class controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+         rb.velocity = new Vector2(0, rb.velocity.y);
+         animator.SetInteger("Estado", 0);
         //posicion 0
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.Z))
         {
-            rb.velocity = new Vector2(velocidad, rb.velocity.y);//que la velocidad cambie
-            sr.flipX = false;
-            if (puedeSaltar != false)
-            {
-                animator.SetInteger("Estado", 1);
-            }
+            rb.velocity = new Vector2(0, rb.velocity.y);
+            animator.SetInteger("Estado", 4);
         }
-        else if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.RightArrow)){
+            rb.velocity = new Vector2(velocidad, rb.velocity.y);
+            animator.SetInteger("Estado", 1);
+            sr.flipX = false;
+            Debug.Log("2");
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             rb.velocity = new Vector2(-velocidad, rb.velocity.y);
             sr.flipX = true;
@@ -44,21 +49,35 @@ public class controller : MonoBehaviour
                 animator.SetInteger("Estado", 1);
             }
         }
-        else if (Input.GetKeyDown(KeyCode.Space) && puedeSaltar)
+        if (Input.GetKey(KeyCode.X) && Input.GetKey(KeyCode.RightArrow))
         {
-            
+            rb.velocity = new Vector2(velocidad*2, rb.velocity.y);//que la velocidad cambie
+            sr.flipX = false;
+            animator.SetInteger("Estado", 2);
+        } 
+         
+        if (Input.GetKey(KeyCode.X) && Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector2(-velocidad * 2, rb.velocity.y);//que la velocidad cambie
+            sr.flipX = true;
+            animator.SetInteger("Estado", 2);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && puedeSaltar)
+        {
             rb.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
             puedeSaltar = false;
             animator.SetInteger("Estado", 3);
-            
+
         }
-        else {
-            if (puedeSaltar != false) {
-                rb.velocity = new Vector2(0, rb.velocity.y);
-                animator.SetInteger("Estado", 0);
-            }
-                
-        }
+        
+        //if (puedeSaltar != false)
+        //   {
+        //       rb.velocity = new Vector2(0, rb.velocity.y);
+        //       animator.SetInteger("Estado", 0);
+        //   }
+
+
     }
 
     private void OnCollisionEnter2D(Collision2D other) // si hay colision  entonces entra aqui 
